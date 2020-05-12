4e8807e4e8342a98dba90f6a0685adba8e3e0e60
EC2 Default User <ec2-user@ip-172-31-94-56.ec2.internal>
2020-05-12 09:47:55 +0000

    added hideslides function and finish quiz button

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,6 +1,33 @@
+var amount;
+var questions;
+var incorrectAns;
+var correctAns;
+
+function hideUnusedSlides(){
+    if (amount == 15) {
+        document.getElementById("slide-20").style.visibility = "hidden";
+        document.getElementById("slide-19").style.visibility = "hidden";
+        document.getElementById("slide-18").style.visibility = "hidden";
+        document.getElementById("slide-17").style.visibility = "hidden";
+        document.getElementById("slide-16").style.visibility = "hidden";
+    }else if(amount == 10){
+        document.getElementById("slide-15").style.visibility = "hidden";
+        document.getElementById("slide-14").style.visibility = "hidden";
+        document.getElementById("slide-13").style.visibility = "hidden";
+        document.getElementById("slide-12").style.visibility = "hidden";
+        document.getElementById("slide-11").style.visibility = "hidden";
+    }else if(amount == 5) {
+        document.getElementById("slide-10").style.visibility = "hidden";
+        document.getElementById("slide-9").style.visibility = "hidden";
+        document.getElementById("slide-8").style.visibility = "hidden";
+        document.getElementById("slide-7").style.visibility = "hidden";
+        document.getElementById("slide-6").style.visibility = "hidden";
+    }
+}
+
 function apiRequest(type, cb) {
 
-    var amount = sessionStorage.getItem('amount');
+    amount = sessionStorage.getItem('amount');
     var category = sessionStorage.getItem('category');
     var difficulty = sessionStorage.getItem('difficulty');
     var type = sessionStorage.getItem('type');
@@ -22,21 +49,29 @@ function getQuestionsAndAns(type) {
         data = data.results;
         console.log(data);
         var rObj = data.map(obj => {
-            var questions = {};
+            questions = {};
             questions[obj.key] = obj.question;
             console.log(questions);
         });
         rObj = data.map(obj => {
-            var incorrectAns = {};
+            incorrectAns = {};
             incorrectAns[obj.key] = obj.incorrect_answers;
-            console.log(incorrectAns);
+
         });
         rObj = data.map(obj => {
-            var correctAns = {};
+            correctAns = {};
             correctAns[obj.key] = obj.correct_answer;
-            console.log(correctAns);
+
         });
     });
 }
+
+
+setTimeout(function() {
+    console.log(amount);
+    console.log(questions);
+    console.log(incorrectAns);
+    console.log(correctAns);
+}, 2000);
 
 
