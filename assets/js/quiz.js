ea5c336d9739e5b744ed9a1204792b0faa95ce0b
EC2 Default User <ec2-user@ip-172-31-94-56.ec2.internal>
2020-05-14 16:49:44 +0000

    added answer arrays

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -4,14 +4,15 @@ var questions;
 var incorrectAns;
 var correctAns;
 
-function hideUnusedSlides(){
+function hideUnusedSlides() {
     if (amount == 15) {
         document.getElementById("slide-20").remove();
         document.getElementById("slide-19").remove();
         document.getElementById("slide-18").remove();
         document.getElementById("slide-17").remove();
         document.getElementById("slide-16").remove();
-    }else if(amount == 10){
+    }
+    else if (amount == 10) {
         document.getElementById("slide-20").remove();
         document.getElementById("slide-19").remove();
         document.getElementById("slide-18").remove();
@@ -22,7 +23,8 @@ function hideUnusedSlides(){
         document.getElementById("slide-13").remove();
         document.getElementById("slide-12").remove();
         document.getElementById("slide-11").remove();
-    }else if(amount == 5) {
+    }
+    else if (amount == 5) {
         document.getElementById("slide-20").remove();
         document.getElementById("slide-19").remove();
         document.getElementById("slide-18").remove();
@@ -65,33 +67,78 @@ function displayQuestions(type) {
         data = data.results;
         console.log(data);
         data.forEach(function(item) {
-             document.getElementById("question-1").innerHTML = data[0].question;
-             document.getElementById("question-2").innerHTML = data[1].question;
-             document.getElementById("question-3").innerHTML = data[2].question;
-             document.getElementById("question-4").innerHTML = data[3].question;
-             document.getElementById("question-5").innerHTML = data[4].question;
-             document.getElementById("question-6").innerHTML = data[5].question;
-             document.getElementById("question-7").innerHTML = data[6].question;
-             document.getElementById("question-8").innerHTML = data[7].question;
-             document.getElementById("question-9").innerHTML = data[8].question;
-             document.getElementById("question-10").innerHTML = data[9].question;
-             document.getElementById("question-11").innerHTML = data[10].question;
-             document.getElementById("question-12").innerHTML = data[11].question;
-             document.getElementById("question-13").innerHTML = data[12].question;
-             document.getElementById("question-14").innerHTML = data[13].question;
-             document.getElementById("question-15").innerHTML = data[14].question;
-             document.getElementById("question-16").innerHTML = data[15].question;
-             document.getElementById("question-17").innerHTML = data[16].question;
-             document.getElementById("question-18").innerHTML = data[17].question;
-             document.getElementById("question-19").innerHTML = data[18].question;
-             document.getElementById("question-20").innerHTML = data[19].question;
+            if (amount == 5) {
+                document.getElementById("question-1").innerHTML = data[0].question;
+                document.getElementById("question-2").innerHTML = data[1].question;
+                document.getElementById("question-3").innerHTML = data[2].question;
+                document.getElementById("question-4").innerHTML = data[3].question;
+                document.getElementById("question-5").innerHTML = data[4].question;
+            }
+            else if (amount == 10) {
+                document.getElementById("question-1").innerHTML = data[0].question;
+                document.getElementById("question-2").innerHTML = data[1].question;
+                document.getElementById("question-3").innerHTML = data[2].question;
+                document.getElementById("question-4").innerHTML = data[3].question;
+                document.getElementById("question-5").innerHTML = data[4].question;
+                document.getElementById("question-6").innerHTML = data[5].question;
+                document.getElementById("question-7").innerHTML = data[6].question;
+                document.getElementById("question-8").innerHTML = data[7].question;
+                document.getElementById("question-9").innerHTML = data[8].question;
+                document.getElementById("question-10").innerHTML = data[9].question;
+            }
+            else if (amount == 15) {
+                document.getElementById("question-1").innerHTML = data[0].question;
+                document.getElementById("question-2").innerHTML = data[1].question;
+                document.getElementById("question-3").innerHTML = data[2].question;
+                document.getElementById("question-4").innerHTML = data[3].question;
+                document.getElementById("question-5").innerHTML = data[4].question;
+                document.getElementById("question-6").innerHTML = data[5].question;
+                document.getElementById("question-7").innerHTML = data[6].question;
+                document.getElementById("question-8").innerHTML = data[7].question;
+                document.getElementById("question-9").innerHTML = data[8].question;
+                document.getElementById("question-10").innerHTML = data[9].question;
+                document.getElementById("question-11").innerHTML = data[10].question;
+                document.getElementById("question-12").innerHTML = data[11].question;
+                document.getElementById("question-13").innerHTML = data[12].question;
+                document.getElementById("question-14").innerHTML = data[13].question;
+                document.getElementById("question-15").innerHTML = data[14].question;
+            }
+            else if (amount == 20) {
+                document.getElementById("question-1").innerHTML = data[0].question;
+                document.getElementById("question-2").innerHTML = data[1].question;
+                document.getElementById("question-3").innerHTML = data[2].question;
+                document.getElementById("question-4").innerHTML = data[3].question;
+                document.getElementById("question-5").innerHTML = data[4].question;
+                document.getElementById("question-6").innerHTML = data[5].question;
+                document.getElementById("question-7").innerHTML = data[6].question;
+                document.getElementById("question-8").innerHTML = data[7].question;
+                document.getElementById("question-9").innerHTML = data[8].question;
+                document.getElementById("question-10").innerHTML = data[9].question;
+                document.getElementById("question-11").innerHTML = data[10].question;
+                document.getElementById("question-12").innerHTML = data[11].question;
+                document.getElementById("question-13").innerHTML = data[12].question;
+                document.getElementById("question-14").innerHTML = data[13].question;
+                document.getElementById("question-15").innerHTML = data[14].question;
+                document.getElementById("question-16").innerHTML = data[15].question;
+                document.getElementById("question-17").innerHTML = data[16].question;
+                document.getElementById("question-18").innerHTML = data[17].question;
+                document.getElementById("question-19").innerHTML = data[18].question;
+                document.getElementById("question-20").innerHTML = data[19].question;
+            }
         });
-    });           
+        correctAns = [];
+        correctAns = data.map(item => {
+            correctAns[item.key] = item.correct_answer;
+            correctAns.forEach(function() {
+            });
+            
+        });
+        var rObj = data.map(obj => {
+            incorrectAns = [];
+            incorrectAns[obj.key] = obj.incorrect_answers;
+            
+        });
+    });
 }
-
-
-
-
-
 
 
