f1b72685b1d9aaf4b02310f234bb120d28acf642
EC2 Default User <ec2-user@ip-172-31-94-56.ec2.internal>
2020-06-01 14:03:59 +0000

    comments added

--- assets/js/load.js
+++ assets/js/load.js
@@ -2,20 +2,20 @@ function formCompletion() {
     var total = $('select[required]').length;
     var selected = 0;
 
-    if ($('#amount option:selected').data('amount') > 0) {
+    if ($('#amount option:selected').data('amount') > 0) { //increments selected when the amount is picked
         selected++;
     }
-    if ($('#category option:selected').data('category') > 0) {
+    if ($('#category option:selected').data('category') > 0) { //increments selected when the category is picked
         selected++;
     }
     if ($('#difficulty option:selected').data('difficulty') === "easy" || $('#difficulty option:selected').data('difficulty') === "medium" || $('#difficulty option:selected').data('difficulty') === "hard") {
-        selected++;
+        selected++; //increments selected when the difficulty is picked
     }
     
     console.log(total);
     console.log(selected);
     
     if (selected == total) {
-        document.getElementById("start-button").removeAttribute('disabled');
+        document.getElementById("start-button").removeAttribute('disabled'); //removes disabled attribute when selected is equal to total
     }
 }
